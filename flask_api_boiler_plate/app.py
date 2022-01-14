efbad7c690a5c2fc9f39fa5d6e28fb330f8d095a
Amirsalar Heydari <amirsalaar@gmail.com>
2022-01-13 21:31:32 -0800

    Update app

--- flask_api_boiler_plate/app.py
+++ flask_api_boiler_plate/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 
 
-def create_app():
+def create_app(config={}):
     app = Flask(__name__)
     # app.config.from_object("app.config.setting")
     # app.config.from_object("app.config.secure")
     CORS(app, supports_credentials=True)
-
+    app.config = config
     return app
